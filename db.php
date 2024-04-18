8a971ca4fe6d19ccd3cc31493cc6e7774514c1f4
marinodilauro <m.dilauro90@gmail.com>
2024-04-18 14:28:05 +0200

    Refactor database array. Add more movie in database.

--- db.php
+++ db.php
@@ -1,10 +1,18 @@
 <?php
 require_once __DIR__ . './models/Production.php';
 
-$production_list = [];
-
-$productionInstance_1 = new Production("Dune", "english", 7);
-$productionInstance_2 = new Production("Bienvenue chez les Ch'tis", "french", 6);
-$productionInstance_3 = new Production("La vita è bella", "italian", 8);
-
-array_push($production_list, $productionInstance_1, $productionInstance_2, $productionInstance_3);
+$production_list = [
+  $productionInstance_1 = new Production("Dune", "Inglese", 7),
+  $productionInstance_2 = new Production("Bienvenue chez les Ch'tis", "Francese", 7),
+  $productionInstance_3 = new Production("La vita è bella", "Italiano", 8),
+  $productionInstance_4 = new Production("Il Signore degli Anelli: La Compagnia dell'Anello", "Inglese", 9),
+  $productionInstance_5 = new Production("Pulp Fiction", "Inglese", 10),
+  $productionInstance_6 = new Production("La Vita è Bella", "Italiano", 8),
+  $productionInstance_7 = new Production("C'era una volta in America", "Inglese", 9),
+  $productionInstance_8 = new Production("Il Padrino", "Inglese", 10),
+  $productionInstance_9 = new Production("Il Buono, il Brutto, il Cattivo", "Italiano", 9),
+  $productionInstance_10 = new Production("Schindler's List", "Inglese", 9),
+  $productionInstance_11 = new Production("Forrest Gump", "Inglese", 8),
+  $productionInstance_12 = new Production("Fight Club", "Inglese", 9),
+  $productionInstance_13 = new Production("Il Favoloso Mondo di Amélie", "Francese", 10)
+];
