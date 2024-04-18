ff7ab3f2f54eb9217919cdba5a9708ceaf624b10
marinodilauro <m.dilauro90@gmail.com>
2024-04-18 13:38:04 +0200

    Create instances of Production class. Print them on screen.

--- index.php
+++ index.php
@@ -6,3 +6,23 @@ class Production
   public $language;
   public $vote;
 }
+
+$productionInstance_1 = new Production();
+$productionInstance_2 = new Production();
+$productionInstance_3 = new Production();
+
+
+$productionInstance_1->title = "Dune";
+$productionInstance_1->language = "english";
+$productionInstance_1->vote = 7;
+var_dump($productionInstance_1);
+
+$productionInstance_2->title = "Bienvenue chez les Ch'tis";
+$productionInstance_2->language = "french";
+$productionInstance_2->vote = 6;
+var_dump($productionInstance_2);
+
+$productionInstance_3->title = "La vita è bella";
+$productionInstance_3->language = "italian";
+$productionInstance_3->vote = 8;
+var_dump($productionInstance_3);
